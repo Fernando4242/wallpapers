e824b8d667a04c1c8827f45717936c6aa5abc43d
portillofernando92@gmail.com <machonacho42!>
2020-10-29 22:10:26 -0500

    - added music and one more gif

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ var i;
 for (i = 0; i <= 37; i++) {
     $(".img-container").append( "<img src='images/image("+ i +").png'>" );
 }
+
+alert("If you hear music and you want to mute it. Right click on the tab and select 'Mute Site'")
