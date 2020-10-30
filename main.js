60d18dd7cd1985b3abe24cc7cb63eee2b9a05b28
Vulpine <44793173+Fernando4242@users.noreply.github.com>
2020-10-29 22:25:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,3 @@ var i;
 for (i = 0; i <= 37; i++) {
     $(".img-container").append( "<img src='images/image("+ i +").png'>" );
 }
-
-alert("If you hear music and you want to mute it. Right click on the tab and select 'Mute Site'")
