d274d22f62695528f02f343ca3953f6a0ac2d179
Vulpine <44793173+Fernando4242@users.noreply.github.com>
2020-10-29 22:40:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var i;
 for (i = 0; i <= 37; i++) {
-    $(".img-container").append( "<img src='images/image("+ i +").png'>" );
+    $(".img-container").append( "<img loading="lazy" src='images/image("+ i +").png'>" );
 }
